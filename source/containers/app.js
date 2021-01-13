3afb84b4b2f5866409a05c9e425724685d818c5a
DanielBerrioB <daniberrio10@hotmail.com>
2021-01-13 14:25:01 -0500

    first commit

--- source/containers/app.js
+++ source/containers/app.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import HomePage from "./home-page";
 
-export default function App() {
-  return (
-    <View style={styles.container} >
-      <Text>Save the planet 🌳</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const AppContainer = () => (
+  <>
+    <HomePage />
+  </>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
+export default AppContainer;
---
+++ source/containers/home-page.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>🌳 Welcome to CELERIK 🌳</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+export default App;
